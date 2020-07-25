dbc647c83aa07bd900d39f9799dd980740bbc49c
vamsi <vamsi.kuchi@gmail.com>
2020-07-25 00:12:14 -0400

    Unit tests implemented Cleanup needed

--- src/main/java/com/mastercard/travel/handler/ConnectionsHandler.java
+++ src/main/java/com/mastercard/travel/handler/ConnectionsHandler.java
@@ -9,10 +9,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 /**
  * This class will be initiated by Spring Framework and Single ton class.
@@ -23,7 +26,7 @@ import org.slf4j.LoggerFactory;
 @Service
 public class ConnectionsHandler {
 
-    private Map<String, Set<String>> connections = null;
+    private List<Set<String>> connections = null;
     private static final Logger logger = LoggerFactory.getLogger(ConnectionsHandler.class);
 
     private static final BiFunction<Set<String>, Set<String>, Set<String>> mergeConnections = (exsistingConnections, newConncetion) -> {
@@ -32,14 +35,22 @@ public class ConnectionsHandler {
         return allConnections;
     };
 
+    private static final BiPredicate<Set<String>,List<String>> checkRouteCities = (routeCities,inputCities) ->{
+      return routeCities.containsAll(inputCities);
+    };
+
     public static final Predicate<String[]> isValidCities = cities -> {
         return cities != null && cities.length == 2 && cities[0].trim().length() > 0 && cities[1].trim().length() > 0;
+    };
+
+    public static final BiPredicate<String,String> isValidInputCities = (city1,city2) ->{
+      return StringUtils.isEmpty(city1) || StringUtils.isEmpty(city2) ?Boolean.FALSE: Boolean.TRUE;
     };
 
     /**
      * Object Creation will fail, if connections file does not exist ot connections file
      *Throws IOException.
-     * @throws IOException
+     * @throws IOException, throws Exception if input resource reading is failed
      */
     public ConnectionsHandler(@Value("${connections.file}") String filePath) throws IOException {
         logger.debug(" Connection loading completed." + filePath);
@@ -52,10 +63,28 @@ public class ConnectionsHandler {
         }
     }
 
+    /***
+     *  Takes two params as inputs and retuen two cities connection status as True/False
+     *  If one of the param is not available , return false
+     *  If both cities are same, return true
+     *  Method input params are not case sesitive.
+     * @param firstCity
+     * @param secondCity
+     * @return Boolean, if both citis connect True else False
+     */
     public boolean searchConnection(String firstCity, String secondCity) {
-        logger.debug("Connections : " + connections);
-        Set<String> cityConnections = connections.get(firstCity);
-        return cityConnections!=null? cityConnections.contains(secondCity):Boolean.FALSE;
+        boolean flag = Boolean.FALSE;
+        if(isValidInputCities.test(firstCity,secondCity)) {
+            if(firstCity.equalsIgnoreCase(secondCity)) return Boolean.TRUE;
+            List<String> inputCities = Arrays.asList(firstCity.trim().toLowerCase(), secondCity.trim().toLowerCase());
+            for(int count=0;count<connections.size();count++){
+                if(checkRouteCities.test(connections.get(count),inputCities)){
+                    flag =Boolean.TRUE;
+                    break;
+                }
+            }
+        }
+        return flag;
     }
 
 
@@ -65,25 +94,105 @@ public class ConnectionsHandler {
      * 2 way map will allow the search easy and faster
      * 2 way map is duplication of data, heavy on memory.
      * This method will return unmodifiable map.
+     * Alternative : In order to avoid duplicates each route should be made as String and add all those String into Set.
+     * If both citites are found in a single String(route)
      *
      * @param classPathResource ClassPath resource for the given filePath
      * @return Map of City, Connected Cities.
      */
-    private Map<String, Set<String>> loadConnections(ClassPathResource classPathResource) throws IOException {
+    private List<Set<String>> loadConnections(ClassPathResource classPathResource) throws IOException {
         Map<String, Set<String>> connectionMap = new HashMap<>();
+        List<String> routes = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(classPathResource.getInputStream()))) {
             String eachLine = null;
+            List<String> exsistingCities = new ArrayList<>();
             while ((eachLine = reader.readLine()) != null) {
                 String[] cities = eachLine.toLowerCase().split(",");
                 if (isValidCities.test(cities)) {
-                    String firstCity = cities[0].trim();
-                    String secondCity = cities[1].trim();
-                    connectionMap.merge(firstCity, Set.of(secondCity), mergeConnections);
-                    connectionMap.merge(secondCity.trim(), Set.of(firstCity), mergeConnections);
+                    routes = createOrMergeRoutes(cities[0].trim(),cities[1].trim(),exsistingCities,routes);
                 }
             }
-            return Collections.unmodifiableMap(connectionMap);
+
+            List<Set<String>> connectionRoutes = new ArrayList<>();
+
+            routes.forEach(eachRoute ->{
+                connectionRoutes.add(
+                        Arrays.asList(eachRoute.split(","))
+                                .stream()
+                                .filter(s -> s.length() > 0)
+                                .collect(Collectors.toSet()));
+            }
+            );
+            return Collections.unmodifiableList(connectionRoutes);
         }
+
     }
+
+    /***
+     *   All new cities list will be maintained (new cities).
+     *  If both cities(input pair)  are new its a new route
+     *          new route will be saved in the list of routes.
+     *  If at least one of the city is not new city from (new cities) ,
+     *      then no new route needed.
+     *   check the routes list for the city (exsisting city from the pair) and new city to those route(s).
+     *   if new cityis added to multiple routes that mean all those routes are connected.
+     *
+     *   if routes are connected , then all those cities will be merged and created a new route ,
+     *   route which are merged will be deleted.
+     *
+     * @return
+     */
+
+    public List<String> createOrMergeRoutes(String city1, String city2, List<String> exsistingCities, List<String> routes){
+        if( (!exsistingCities.contains(city1)) && (!exsistingCities.contains(city2))){
+            exsistingCities.add(city1);
+            exsistingCities.add(city2);
+            routes.add(city1+","+city2);
+        }else{
+            boolean contains = exsistingCities.contains(city1);
+            boolean b = !exsistingCities.contains(city2);
+            if(contains &&  b) {
+                exsistingCities.add(city2);
+                routes=  mergeRoutes(city1,city2, routes);
+            } else if( (!exsistingCities.contains(city1)) &&  exsistingCities.contains(city2)){
+                exsistingCities.add(city1);
+                routes= mergeRoutes(city2,city1, routes);
+            }else{
+               routes= mergeRoutes(city1,city2, routes);
+               routes=mergeRoutes(city2,city1, routes);
+            }
+        }
+        return routes;
+    }
+
+    private List<String> mergeRoutes(String city1, String city2, List<String> routes) {
+        List<Integer> newCityAddedroutesList = new ArrayList<>();
+        for(int count=0;count<routes.size();count++){
+            if( routes.get(count).contains(city1)){
+                String concat = routes.get(count).concat(",").concat(city2);
+                routes.set(count,concat);
+                newCityAddedroutesList.add(count);
+            }
+        }
+        if(newCityAddedroutesList.size()>1){
+            // new city added to multiple routes,
+            // so all those routes should be merged and new route should be created by merging these routes
+            StringBuilder builder = new StringBuilder();
+            for(int count=0;count<newCityAddedroutesList.size();count++) {
+                builder.append(routes.get(count)).append(",");
+            }
+            List<String> newRoutes = new ArrayList<>();
+
+            for(int count=0;count<routes.size();count++) {
+                if(!newCityAddedroutesList.contains(count)){
+                    newRoutes.add(routes.get(count));
+                }
+            }
+            newRoutes.add(builder.toString());
+            return newRoutes;
+        }
+        return routes;
+    }
+
 
 }
--- src/test/java/com/mastercard/travel/TravelApplicationTests.java
+++ src/test/java/com/mastercard/travel/TravelApplicationTests.java
@@ -1,13 +1,104 @@
 package com.mastercard.travel;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TravelApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+
+	/**
+	 *  test with Blank inputs.
+	 *  chceks status code and response value
+	 */
 	@Test
-	void contextLoads() {
+	public void testBlanks() throws Exception {
+        ResultActions perform = mockMvc.perform(get("/connected?origin=&destination=")).
+				andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("no")));
+    }
+
+	/**
+	 *  test with Same City Names.
+	 *  chceks status code and response value
+	 */
+	@Test
+	public void testSameCitiesConnected() throws Exception {
+		ResultActions perform = mockMvc.perform(get("/connected?origin=Boston&destination=Boston")).
+				andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("yes")));
 	}
+
+	/**
+	 *  test with Same City Names with different Cases.
+	 *  chceks status code and response value
+	 */
+	@Test
+	public void testSameCitiesUpperCaseConnected() throws Exception {
+		ResultActions perform = mockMvc.perform(get("/connected?origin=BOSTON&destination=boston")).
+				andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("yes")));
+	}
+
+	/**
+	 *  test with Same City Names with POST.
+	 *  chceks status code as Method not allowed
+	 */
+	@Test
+	public void testCitiesConnectedWithPost() throws Exception {
+		ResultActions perform = mockMvc.perform(post("/connected?origin=BOSTON&destination=boston")).
+				andDo(print()).andExpect(status().isMethodNotAllowed());
+		//dExpect(content().string(containsString("")));
+	}
+
+	/**
+	 *  test with Same City Names with Put.
+	 *  chceks status code as Method not allowed
+	 */
+	@Test
+	public void testCitiesConnectedWithPut() throws Exception {
+		ResultActions perform = mockMvc.perform(put("/connected?origin=BOSTON&destination=boston")).
+				andDo(print()).andExpect(status().isMethodNotAllowed());
+		//dExpect(content().string(containsString("")));
+	}
+
+	/**
+	 *  test with Same City Names with Delete.
+	 *  chceks status code as Method not allowed
+	 */
+	@Test
+	public void testCitiesConnectedWithDelete() throws Exception {
+		ResultActions perform = mockMvc.perform(delete("/connected?origin=BOSTON&destination=boston")).
+				andDo(print()).andExpect(status().isMethodNotAllowed());
+		//dExpect(content().string(containsString("")));
+	}
+
+	/**
+	 *  test with cities connection
+	 *  chceks status code and response value
+	 */
+	@Test
+	public void testDifferentCitiesConnection() throws Exception {
+		ResultActions perform = mockMvc.perform(get("/connected?origin=Boston&destination=Albany")).
+				andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("yes")));
+	}
+
+
+
 
 }
